b2341ab20836ab5fcbe2dd0f8a04c6311eeb6ab2
arcanlte <pinangayjoel@gmail.com>
2020-12-28 22:40:52 -0500

    property access

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,38 @@
+// const myBook = document.querySelector("h1");
+// const myWriter = document.querySelector("p");
+// myBook.textContent = object.Book;
+// myWriter.textContent = object.Writer;
+
+//function literal
 var object = {
-  Book: "The Principles of Object-Oriented Javascript",
-  Writer: "Nicholas C. Zakas",
+  book: "The Principles of Object-Oriented Javascript",
+  writer: "Nicholas C. Zakas",
 };
 
-const myHeading = document.querySelector("h1");
-myHeading.textContent = object.Writer;
+console.log(object);
+
+//constructor
+var object1 = new Object();
+object1.book = "Game of Thrones";
+object1.writer = "George R. R. Martin";
+object.BookType = "Hard Bound";
+
+console.log(object1);
+
+//property access
+
+//dot notation
+var arr = [];
+arr.push(123);
+
+console.log(arr);
+
+var array = [];
+var method;
+method = "push";
+array[method]([123], [1]);
+method = "pop";
+array[method]();
+console.log(array);
+
+object = null;
