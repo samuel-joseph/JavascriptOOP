fd28c02106a0494f562506b2939b6a1e18812b08
arcanlte <pinangayjoel@gmail.com>
2020-12-28 21:45:42 -0500

    styles.css initiated

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var object = {
+  Book: "The Principles of Object-Oriented Javascript",
+  Writer: "Nicholas C. Zakas",
+};
+
+const myHeading = document.querySelector("h1");
+myHeading.textContent = object.Writer;
